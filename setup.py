90c839a054b1d806f9a765c2183e0b8c73fba191
Alex Hall <alex@businessoptics.biz>
2015-08-07 15:15:58 +0200

    Updated setup.py url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='asdb',
       version='0.1',
       description='Instant convenient debugger',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/alexmojaki/asdb',
       author='Alex Hall',
       author_email='alex.mojaki@gmail.com',
       license='MIT',
