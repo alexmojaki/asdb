4071f4f682a1c13aac4702247292cb3057a23b66
Alex Hall <alex.mojaki@gmail.com>
2017-03-30 23:58:59 +0200

    Defer to littleutils

--- asdb/__init__.py
+++ asdb/__init__.py
@@ -2,9 +2,7 @@ import socket
 import errno
 import sys
 from os.path import join
-import re
-import inspect
-from pprint import pprint
+from littleutils import magic_print
 
 
 def find_port(host="127.0.0.1", port=6950, search_limit=100):
@@ -62,100 +60,3 @@ set_trace = t
 
 class NoAvailablePortException(Exception):
     pass
-
-
-class _MagicPrinter(object):
-    """
-    >>> a = 'hello world how are you today'
-    >>> b = [{1: 2}, a, [3] * 20]
-    >>> magic_print('a b', x=1+2)
-    <BLANKLINE>
-    =================== a : ===================
-    <BLANKLINE>
-    'hello world how are you today'
-    <BLANKLINE>
-    <BLANKLINE>
-    =================== b : ===================
-    <BLANKLINE>
-    [{1: 2},
-     'hello world how are you today',
-     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]]
-    <BLANKLINE>
-    <BLANKLINE>
-    =================== x : ===================
-    <BLANKLINE>
-    3
-    <BLANKLINE>
-    >>> magic_print.a.b  # doctest:+ELLIPSIS
-    <BLANKLINE>
-    =================== a : ===================
-    <BLANKLINE>
-    'hello world how are you today'
-    <BLANKLINE>
-    <BLANKLINE>
-    =================== b : ===================
-    <BLANKLINE>
-    [{1: 2},
-     'hello world how are you today',
-     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]]
-    <BLANKLINE>
-    <...._MagicPrinter object at ...>
-    """
-
-    @staticmethod
-    def _print_variable(name):
-        value = helpful_error_dict_get(inspect.currentframe().f_back.f_back.f_locals, name)
-        _MagicPrinter._print_named_value(name, value)
-
-    @staticmethod
-    def _print_named_value(name, value):
-        print('\n=================== %s : ===================\n' % name)
-        pprint(value)
-        print('')
-
-    def __getattr__(self, item):
-        try:
-            self._print_variable(item)
-        except KeyError as e:
-            raise AttributeError(e.message)
-        return self
-
-    def __call__(self, names, **kwargs):
-        for name in ensure_list_if_string(names):
-            self._print_variable(name)
-        for name, value in kwargs.items():
-            self._print_named_value(name, value)
-
-
-magic_print = _MagicPrinter()
-
-
-def _helpful_dict_error(d, key):
-    raise KeyError('Tried to access %r, only keys are: %s' % (key, str(sorted(d.keys()))[:1000]))
-
-
-def helpful_error_dict_get(d, key):
-    """
-    >>> d = {1: 2, 3: 4}
-    >>> helpful_error_dict_get(d, 'a')
-    Traceback (most recent call last):
-    ...
-    KeyError: "Tried to access 'a', only keys are: [1, 3]"
-    """
-    try:
-        return d[key]
-    except KeyError:
-        _helpful_dict_error(d, key)
-
-
-def ensure_list_if_string(x):
-    """
-    >>> assert (ensure_list_if_string(['abc', 'def', '123']) ==
-    ...         ensure_list_if_string('abc,def,123') ==
-    ...         ensure_list_if_string('abc def 123') ==
-    ...         ensure_list_if_string(', abc , def , 123  , ,') ==
-    ...         ['abc', 'def', '123'])
-    """
-    if isinstance(x, basestring):
-        x = list(filter(None, re.split('[,\s]+', x)))
-    return x
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='asdb',
-      version='0.3.0',
+      version='0.3.1',
       description='Instant convenient debugger',
       url='https://github.com/alexmojaki/asdb',
       author='Alex Hall',
       author_email='alex.mojaki@gmail.com',
       license='MIT',
       packages=['asdb'],
-      install_requires=['rpdb'],
+      install_requires=['rpdb', 'littleutils'],
       scripts=['bin/asdb'],
       zip_safe=False)
